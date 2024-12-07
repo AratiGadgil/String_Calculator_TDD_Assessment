bf0f7d9b807409e0fec33554b71086e64e5f5420
Arati <arati.gadgil1@gmail.com>
2024-12-07 23:23:26 +0530

    Added test case for multiple numbers in input string

--- add_function.js
+++ add_function.js
@@ -1,3 +1,7 @@
 export function add(numbers) {
-    if (!numbers) return 0;
-  }
+  if (!numbers) return 0;
+  let numArr = numbers.split(",");
+  console.log("numArr: ", numArr);
+  let sum = numArr.reduce((total, number) => total + parseInt(number), 0);
+  return sum;
+}
--- test/add_function.spec.js
+++ test/add_function.spec.js
@@ -1,8 +1,27 @@
 import { assert } from "chai";
 import { add } from "../add_function.js";
-describe("add function with empty string", () => {
+
+describe("add function with input as empty string", () => {
   it("should return 0 for empty string", () => {
     const result = add("");
     assert.equal(result, 0);
+  });
+});
+describe("add function with one number in input string", () => {
+  it("should return number in the string", () => {
+    const result = add("1");
+    assert.equal(result, 1);
+  });
+});
+describe("add function with two numbers in input string", () => {
+  it("should return sum of numbers in the string", () => {
+    const result = add("1,2");
+    assert.equal(result, 3);
+  });
+});
+describe("add function with multiple numbers in input string", () => {
+  it("should return sum of numbers in the string", () => {
+    const result = add("1,2,3");
+    assert.equal(result, 6);
   });
 });
